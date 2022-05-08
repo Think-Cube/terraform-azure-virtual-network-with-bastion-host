c7157fb44d1c19ec7cfa66e62fe16cdec94697ea
spy86 <contact@mmichalski.com>
2022-05-08 12:43:56 +0200

    fix wrong var name, update readme.md

--- bastion_host.tf
+++ bastion_host.tf
@@ -1,5 +1,5 @@
 resource "azurerm_bastion_host" "main" {
-  name                = "${var.environment}-${var.bastionhost_name}-${var.region}-bas"
+  name                = "${var.environment}-${var.bastion_hostname}-${var.region}-bas"
   location            = "${data.azurerm_resource_group.rg.location}"
   resource_group_name = "${data.azurerm_resource_group.rg.name}"
   tags                = "${var.default_tags}"
--- publicip.tf
+++ publicip.tf
@@ -1,5 +1,5 @@
 resource "azurerm_public_ip" "main" {
-  name                = "${var.environment}-${var.bastionhost_name}-${var.region}-PubIP"
+  name                = "${var.environment}-${var.bastion_hostname}-${var.region}-PubIP"
   location            = "${data.azurerm_resource_group.rg.location}"
   resource_group_name = "${data.azurerm_resource_group.rg.name}"
   allocation_method   = "${var.pubip_allocation_method}"
