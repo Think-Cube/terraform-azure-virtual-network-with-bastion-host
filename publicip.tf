e5b30111c4bc0b41b1f2b622395a3abd722e96b1
Maciej Michalski <5445923+spy86@users.noreply.github.com>
2025-01-30 14:02:23 +0000

    Update publicip.tf

--- publicip.tf
+++ publicip.tf
@@ -1,7 +1,7 @@
 resource "azurerm_public_ip" "main" {
-  name                = "${var.environment}-${var.bastion_hostname}-${var.region}-pip"
-  location            = data.azurerm_resource_group.rg.location
-  resource_group_name = data.azurerm_resource_group.rg.name
+  name                = "${var.environment}-${var.public_ip_name}"
+  location            = data.azurerm_resource_group.bastion.location
+  resource_group_name = data.azurerm_resource_group.bastion.name
   allocation_method   = var.pubip_allocation_method
   sku                 = var.pubip_sku
   tags                = var.default_tags
